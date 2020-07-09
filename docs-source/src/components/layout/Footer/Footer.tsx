78ed4f16845ae9039931e2d19cb64c99afa6e096
Nikita Bukin <me@z3sa.ru>
2020-07-10 01:36:28 +0800

    build(docs): fixed images in docs site

--- docs-source/src/components/layout/Footer/Footer.tsx
+++ docs-source/src/components/layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className={styles.Footer}>
       <a target="_blank" href="https://github.com/Z3SA/thundercreate">
-        <img src="/images/github.png" />
+        <img src={`${process.env.linkPrefix}/images/github.png`} />
       </a>
     </footer>
   );
